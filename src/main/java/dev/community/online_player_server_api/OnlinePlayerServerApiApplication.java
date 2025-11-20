8be4125772bec920d5cfafc10a489fbcf89861d1
cristian <cristian.raileanu@rwth-aachen.de>
2025-11-20 23:53:15 +0200

    add spring boot stub

---
+++ src/main/java/dev/community/online_player_server_api/OnlinePlayerServerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.community.online_player_server_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePlayerServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePlayerServerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/community/online_player_server_api/OnlinePlayerServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.community.online_player_server_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePlayerServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
